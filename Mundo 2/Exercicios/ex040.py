05b3be96a1532686c4452de62adafcaf3eb69c5f
Gabriel <gabriel@mpsouza.com.br>
2022-03-14 14:09:31 -0300

    1.2.0

--- Mundo 2/Exercicios/ex040.py
+++ Mundo 2/Exercicios/ex040.py
@@ -1,1 +1,10 @@
 print('='*12 + ' DESAFIO 40 ' + '='*12)
+n1 = int(input('Digite sua 1º nota: '))
+n2 = int(input('Digite sua 2º nota: '))
+media = (n1 + n2)/2
+if media < 5.0:
+    print('\033[1;31m ---== REPROVADO ==--- \033[m')
+elif 5.0 <= media <= 6.9:
+    print('\033[1;33m ---== RECUPERAÇÃO ==--- \033[m')
+else:
+    print('\033[1;32m ---== APROVADO ==--- \033[m')
---
+++ Mundo 2/Exercicios/ex41.py
@@ -0,0 +1,15 @@
+print('='*12 + ' DESAFIO 41 ' + '='*12)
+from datetime import date
+ano = int(input('Digite seu ano de nascimento: '))
+idade = date.today().year - ano
+if idade <= 9:
+    classe = 'Mirim'
+elif 9 < idade <= 14:
+    classe = 'Infantil'
+elif 14 < idade <= 19:
+    classe = 'Junior'
+elif 19 < idade <= 20:
+    classe = 'Sênior'
+else:
+    classe = 'Master'
+print('Sua classificação é {}.'.format(classe))
