1fffee3cb674be31b6ec4cf6a4997d45380c739a
Gabriel <gabriel@mpsouza.com.br>
2022-03-17 14:02:06 -0300

    2.12.41

--- Mundo 2/Exercicios/ex41.py
+++ Mundo 2/Exercicios/ex41.py
@@ -2,15 +2,15 @@ print('='*12 + ' DESAFIO 41 ' + '='*12)
 from datetime import date
 ano = int(input('Digite seu ano de nascimento: '))
 idade = date.today().year - ano
+print('O atleta tem {} anos.'.format(idade))
 if idade <= 9:
     classe = 'Mirim'
 elif 9 < idade <= 14:
     classe = 'Infantil'
 elif 14 < idade <= 19:
     classe = 'Junior'
-elif 19 < idade <= 20:
+elif 19 < idade <= 25:
     classe = 'Sênior'
 else:
     classe = 'Master'
-print('Sua classificação é {}.'.format(classe))
-
+print('Classificação: \033[1m{}\033[m'.format(classe))
