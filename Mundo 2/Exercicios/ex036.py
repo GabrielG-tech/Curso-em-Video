c0e0cfb17a46fc305ee463c3b56ec0c1d59deaa9
Gabriel <gabriel@mpsouza.com.br>
2022-03-17 08:05:20 -0300

    2.12.36

--- Mundo 2/Exercicios/ex036.py
+++ Mundo 2/Exercicios/ex036.py
@@ -4,11 +4,11 @@ salario = int(input('Salario do comprador: R$'))
 anos = int(input('Por quantos anos deseja parcelar: '))
 prest = casa / (anos * 12)
 if prest <= salario * 0.3:
-    print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end='')
+    print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end=' ')
     print('a prestação será de R${:.2f}.'.format(prest))
     print('Empréstimo foi \033[1;32mconcedido\033[m! por mês.')
 else:
-    print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end='')
+    print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end=' ')
     print('a prestação será de R${:.2f}.'.format(prest))
     print('Empréstimo foi \033[1;31mnegado\033[m!')
 
