cde4e6ec8bb2232365120d76fa579dc7d70c995c
Gabriel <gabriel@mpsouza.com.br>
2022-03-17 08:48:18 -0300

    2.12.38

--- Mundo 2/Exercicios/ex038.py
+++ Mundo 2/Exercicios/ex038.py
@@ -1,10 +1,12 @@
 print('='*12 + ' DESAFIO 38 ' + '='*12)
-pv = int(input('Escreva o primeiro valor para comparação: '))
-sv = int(input('Escreva o segundo valor para comparação: '))
-lista = [pv, sv]
-if pv == sv:
+n1 = int(input('Escreva o primeiro valor para comparação: '))
+n2 = int(input('Escreva o segundo valor para comparação: '))
+lista = [n1, n2]
+if n1 == n2:
     print('Não existe valor maior, os dois são iguais.')
+elif n1 > n2:
+    print('O Primeiro valor ({}) é o maior.'.format(max(lista)))
+elif n2 > n1:
+    print('O Segundo valor ({}) é o maior.'.format(max(lista)))
 else:
-    print('O {} é o maior número'.format(max(lista)))
-    print('O {} é o menor número'.format(min(lista)))
-
+    print('Não existe valor maior, os dois são iguais.')
