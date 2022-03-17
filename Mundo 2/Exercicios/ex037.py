ba0bab9417ee49d2b74c0661db4f9f9e6b8b5ee9
Gabriel <gabriel@mpsouza.com.br>
2022-03-17 08:04:55 -0300

    2.12.37

--- Mundo 2/Exercicios/ex037.py
+++ Mundo 2/Exercicios/ex037.py
@@ -1,17 +1,15 @@
 print('='*12 + ' DESAFIO 37 ' + '='*12)
-num = int(input('Número para conversão: '))
-sist = int(input('Digite [1] para Binário, [2] para Octal e [3] para Hexadecimal: '))
-if sist == 1:
-    n = bin(num)
-    print(n)
-    x = 'Binário'
-elif sist == 2:
-    n = oct(num)
-    print(n)
-    x = 'Octal'
-elif sist == 3:
-    n = hex(num)
-    print(n)
-    x = 'Hexadecimal'
-print('O número {} é igual a {} em {}'.format(num, n, x))
-
+num = int(input('Número um número inteiro para conversão: '))
+print('''Escolha uma das bases para conversão
+[ 1 ] converter para Binário
+[ 2 ] converter para Octal
+[ 3 ] converter para Hexadeciamal''')
+opção = int(input('Sua opção: '))
+if opção == 1:
+    print('{} convertido para Binário é igual a {}.'.format(num, bin(num)[2:]))
+elif opção == 2:
+    print('{} convertido para Octal é igual a {}.'.format(num, oct(num)[2:]))
+elif opção == 3:
+    print('{} convertido para Hexadeciamal é igual a {}.'.format(num, hex(num)[2:]))
+else:
+    print('\033[31m*Opção inválida, Tente Novamente*\033[m')
