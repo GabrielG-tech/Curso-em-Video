b7e5efa3f85e3cd41ed57a1a6373807decfb8ce6
Gabriel <gabriel@mpsouza.com.br>
2022-03-17 14:42:22 -0300

    2.12.43

--- Mundo 2/Exercicios/ex43.py
+++ Mundo 2/Exercicios/ex43.py
@@ -6,14 +6,14 @@ peso = float(input('Digite seu peso (kg): '))
 altura = float(input('Digite sua altura (m): '))
 IMC = peso/(altura**2)
 if IMC < 18.5:
-    result = 'Abaixo do Peso'
+    result = '\033[4;33mabaixo do Peso\033[m.'
 elif 18.5 <= IMC <= 25:
-    result = 'Peso ideal'
+    result = 'de \033[32mPeso ideal\033[m.'
 elif 25 < IMC <= 30:
-    result = 'Sobrepeso'
+    result = 'de \033[33mSobrepeso\033[m.'
 elif 30 < IMC <= 40:
-    result = 'Obesidade'
+    result = 'de \033[4;31mObesidade\033[m.'
 else:
-    result = 'Obesidade mórbida'
-print('Seu caso é de: {} [{:.2f}kg/m²].'.format(result, IMC))
-
+    result = '\033[1;31mObesidade mórbida, Cuidado!\033[m'
+print('Seu IMC (Índice de Massa Corporal) é de {:.2f} kg/m².'.format(IMC))
+print('Você esta na faixa {}'.format(result))
