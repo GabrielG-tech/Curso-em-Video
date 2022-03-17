3fba3b3e8c0c4bacc9fb93257fc2e1ec1f35ad96
Gabriel <gabriel@mpsouza.com.br>
2022-03-17 13:36:45 -0300

    2.12.39

--- Mundo 2/Exercicios/ex039.py
+++ Mundo 2/Exercicios/ex039.py
@@ -1,11 +1,28 @@
 print('='*12 + ' DESAFIO 39 ' + '='*12)
-from datetime import date
-ano = int(input('Ano de nascimento: '))
-idade = date.today().year - ano
-if idade < 18:
-    print('Ainda não é hora de se alistar, Ainda faltam {} anos.'.format(18 - idade))
-elif idade == 18:
-    print('É hora de se alistar.')
+print('''Digite o corespondente do seu sexo: 
+[ 1 ] Masculino
+[ 2 ] Feminino''')
+sexo = int(input('Resposta: '))
+if sexo == 2:
+    print('Você não precisa se alistar obrigatório.')
 else:
-    print('Já passou da hora de se alistar faz {} anos.'.format(idade - 18))
-
+    from datetime import date
+    ano = int(input('Ano de nascimento: '))
+    idade = date.today().year - ano
+    if idade < 18:
+        if (18 - idade) != 1:
+            s = 's'
+            m = 'm'
+        else:
+            s = ''
+            m = ''
+        print('Ainda falta{} {} ano{}. Será em {}.'.format(m, 18 - idade, s, 18 + ano))
+    elif idade == 18:
+        print('É hora de se alistar agora em {}.'.format(date.today().year))
+    else:
+        if (idade - 18) != 1:
+            s = 's'
+        else:
+            s = ''
+        print('Você já deveria ter se alistado faz {} ano{}.'.format(idade - 18, s))
+        print('Deveria ter sido em {}.'.format(date.today().year - (idade - 18)))
