4fd58f3761824caa47e1d3bdd17356eb042c18fc
Gabriel <gabriel@mpsouza.com.br>
2022-03-18 15:17:34 -0300

    2.12.44

--- Mundo 2/Exercicios/ex44.py
+++ Mundo 2/Exercicios/ex44.py
@@ -1,17 +1,24 @@
-print('='*12 + ' DESAFIO 44 ' + '='*12)
-preço = float(input('Preço do produto: '))
+print('{:=^40}'.format(' DESAFIO 44 '))
+preço = float(input('Preço do produto: R$'))
 print('''Escreva o número correspondente a formas de pagamento:
-- Dinheiro ou chegue (10% de desconto) [1]
-- À vista no cartão (5% de desconto) [2]
-- Em até 2x no cartão [3]
-- 3x ou mais no cartão (20% de juros) [4]''')
-forma_de_pagamento = int(input('Formar de pagamento: '))
-if forma_de_pagamento == 1:
-    valor = preço - preço * 10 / 100
-elif forma_de_pagamento == 2:
-    valor = preço - preço * 5 / 100
-elif forma_de_pagamento == 3:
-    valor = preço
-elif forma_de_pagamento == 4:
-    valor = preço + preço * 20 / 100
-print('\nO produto custará \033[1;32mR${:.2f}\033[m.'.format(valor))
+[ 1 ] Dinheiro ou chegue
+[ 2 ] À vista no cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opção = int(input('Opção: '))
+if opção == 1:
+    total = preço - (preço * 10 / 100)
+elif opção == 2:
+    total = preço - (preço * 5 / 100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R${:.2f}'.format(parcela))
+elif opção == 4:
+    total = preço + (preço * 20 / 100)
+    totparc = int(input('Quantidade de parcelas: '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R${:.2f} com \033[1mjuros\033[m.'.format(totparc, parcela))
+else:total = preço
+print('\033[1;31mOpção inválidade de pagamento. Tente Novamente!\033[m')
+print('Sua compra de {}, custará no total \033[1mR${:.2f}\033[m.'.format(preço, total))
