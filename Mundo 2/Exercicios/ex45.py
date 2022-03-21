9f38803bfe81dffce381220d619704e82bd23cb9
Gabriel <gabriel@mpsouza.com.br>
2022-03-21 17:22:26 -0300

    2.12.45

--- Mundo 2/Exercicios/ex45.py
+++ Mundo 2/Exercicios/ex45.py
@@ -1,26 +1,47 @@
 print('='*12 + ' DESAFIO 45 ' + '='*12)
 from random import randint
+from time import sleep
 itens = ('Pedra', 'Papel', 'Tesoura')
 bot = randint(0,2)
-print('-='*20)
-print('         Vamos jogar Jokenpô!')
-print('-='*20)
-print(' [ 0 ] para Pedra\n [ 1 ] para Papel\n [ 2 ] para Tesoura')
-jogo = int(input('Faça sua jogada: '))
-print('Bot jogou {}'.format(itens[bot]))
-print('Jogador jogou {}'.format(itens[jogo]))
-#if jogo == 1 and bot == 1 or jogo == 2 and bot == 2 or jogo == 3 and bot == 3:
-#    resultado = '\033[1;33mEmpate, Bom jogo!\033[m'
-#elif jogo == 1 and bot == 2 or jogo == 2 and bot == 3 or jogo == 3 and bot == 1:
-#    resultado = '\033[1;31mVitoria do Bot, Bom jogo!\033[m'
-#elif jogo == 2 and bot == 1 or jogo == 3 and bot == 2 or jogo == 1 and bot == 3:
-#    resultado = '\033[1;32mVitória do Jogador, Parabéns!!\033[m'
-#if bot == 1: botx = 'Pedra'
-#elif bot == 2: botx = 'Papel'
-#elif bot == 3: botx = 'Tesoura'
-#print('Jo', 'ken', 'pô')
-#print('-=' * 11)
-#print('''Computador jogou {}
-#Jogador jogou {}'''.format(botx, jogo))
-#print('-=' * 11)
-#print(resultado)
+print('-='*12)
+print('  Vamos jogar Jokenpô!')
+print('-='*12)
+print('[ 0 ] para Pedra\n[ 1 ] para Papel\n[ 2 ] para Tesoura')
+jogador = int(input('Faça sua jogada: '))
+print('Jo')
+sleep(0.8)
+print('ken')
+sleep(0.8)
+print('po!!!')
+sleep(0.8)
+print('-='*12)
+print('Jogador jogou {}'.format(itens[jogador]))
+print('Computador jogou {}'.format(itens[bot]))
+print('-='*12)
+if bot == 0:
+    if jogador == 0:
+        print('\033[1;33mEmpate, Bom jogo!\033[m')
+    elif jogador == 1:
+        print('\033[1;32mVitória do Jogador, Parabéns!!\033[m')
+    elif jogador == 2:
+        print('\033[1;31mVitoria do Bot, Bom jogo!\033[m')
+    else:
+        print('\033[1;31mJogada inválida, tente novamente.\033[m')
+elif bot == 1:
+    if jogador == 0:
+        print('\033[1;31mVitoria do Bot, Bom jogo!\033[m')
+    elif jogador == 1:
+        print('\033[1;33mEmpate, Bom jogo!\033[m')
+    elif jogador == 2:
+        print('\033[1;32mVitória do Jogador, Parabéns!!\033[m')
+    else:
+        print('\033[1;31mJogada inválida, tente novamente.\033[m')
+elif bot == 2:
+    if jogador == 0:
+        print('\033[1;32mVitória do Jogador, Parabéns!!\033[m')
+    elif jogador == 1:
+        print('\033[1;31mVitoria do Bot, Bom jogo!\033[m')
+    elif jogador == 2:
+        print('\033[1;33mEmpate, Bom jogo!\033[m')
+    else:
+        print('\033[1;31mJogada inválida, tente novamente.\033[m')
