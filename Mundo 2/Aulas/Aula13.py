6b7392446989b8651ca183f1892367636ccc8ebc
Gabriel <gabriel@mpsouza.com.br>
2022-03-22 16:00:06 -0300

    2.13.0

--- Mundo 2/Aulas/Aula13.py
+++ Mundo 2/Aulas/Aula13.py
@@ -1,5 +1,22 @@
 from time import sleep
-for c in range(5, 0, -1):
-    print(c)
-    sleep(0.4)
-print('\033[1;36mFim\033[m')
+#n = int(input('Escreva um número: '))
+#for c in range(n, 0, -1):
+#    print(c)
+#    sleep(0.4)
+#print('\033[1;31mFim\033[m')
+
+#i = int(input('Início: '))
+#f = int(input('Fim: '))
+#p = int(input('Passo: '))
+#for c in range(i, f+1, p):
+#    print(c)
+#    sleep(0.8)
+#print('Fim')
+
+s = 0
+for c in range(0, 4):
+    sleep(0.8)
+    n = int(input('Digite um valor: '))
+#    s = s + n
+    s += n
+print('O somatório de todos os números é {}.'.format(s))
