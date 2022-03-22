73eae4e551a4c0bdd568c5168d2435097918f1a6
Gabriel <gabriel@mpsouza.com.br>
2022-03-22 16:31:11 -0300

    2.13.46

--- Mundo 2/Exercicios/ex46.py
+++ Mundo 2/Exercicios/ex46.py
@@ -1,6 +1,9 @@
 print('{:=^40}'.format(' DESAFIO 46 '))
 from time import sleep
-for c in range(10, 0, -1):
+import emoji
+for c in range(3, 0, -1):
     print(c)
     sleep(1)
-print('\033[1;31m* * * * Fogos de artifício * * * *\033[m')
+print(emoji.emojize("\033[1;31m:fireworks: \033[m") * 21)
+print('\033[1;31m{}Fogos de artifício {}\033[m'.format(emoji.emojize(":fireworks: ") * 7, emoji.emojize(":fireworks: ") * 7))
+print(emoji.emojize("\033[1;31m:fireworks: \033[m") * 21)
