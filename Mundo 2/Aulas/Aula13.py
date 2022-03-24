340ebbeec370c645e51d45bc284f3c9a39b774d9
Gabriel <gabriel@mpsouza.com.br>
2022-03-23 22:14:25 -0300

    2.13.0

--- Mundo 2/Aulas/Aula13.py
+++ Mundo 2/Aulas/Aula13.py
@@ -17,6 +17,6 @@ s = 0
 for c in range(0, 4):
     sleep(0.8)
     n = int(input('Digite um valor: '))
-#    s = s + n
+#   s = s + n
     s += n
 print('O somatório de todos os números é {}.'.format(s))
