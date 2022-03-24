e625990961683430ecd718bdbf992afe4bf3a785
Gabriel <gabriel@mpsouza.com.br>
2022-03-23 23:44:54 -0300

    2.13.51.0

---
+++ Mundo 2/Exercicios/ex51.py
@@ -0,0 +1,5 @@
+print('{:=^40}'.format(' DESAFIO 51 '))
+n = float(input('Digite um número para a progressão: '))
+r = int(input('Digite a razão da P.A.: '))
+for c in range(n, 10, r):
+    print(c)
