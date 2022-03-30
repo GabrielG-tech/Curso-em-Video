78e9caca5124d3c03d18f5f6dd3860862460cef6
Gabriel <gabriel@mpsouza.com.br>
2022-03-30 20:59:03 -0300

    2.13.53

--- Mundo 2/Exercicios/ex53.py
+++ Mundo 2/Exercicios/ex53.py
@@ -1,10 +1,12 @@
 print('='*14 + ' DESAFIO 53 ' + '='*14)
-n = str(input('Digite uma frase para análise: '))
-n = n.lower().strip().split()
-n = ''.join(n)
-if len(n) % 2 == 0:
-    print(n.split(n))
-#    resultado = 'Esta frase é um palindromo.'
+frase = str(input('Digite uma frase para análise: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+#for letra in range(len(junto)-1, -1, -1):
+#    inverso += junto[letra]
+print('O inverso de {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palindromo.')
 else:
-    print('Fail')
-#    resultado = 'Esta frase não é um palindromo.'
+    print('A frase digitada não é um palindromo.')
