468a52032dd8ea9f22b0cc62ca213251984de11a
Gabriel <gabriel@mpsouza.com.br>
2022-03-30 19:42:48 -0300

    2.13.50

--- Mundo 2/Exercicios/ex50.py
+++ Mundo 2/Exercicios/ex50.py
@@ -1,8 +1,10 @@
 print('='*12 + ' DESAFIO 50 ' + '='*12)
-s = 0
+soma = 0
+cont = 0
 for c in range(0, 6):
     num = int(input('Digite um número: '))
     if num % 2 == 0:
-        s += num
-    print('Soma de todos os pares até agora: {}'.format(s))
-print('A soma de todos os números pares digitados é {}.'.format(s))
+        soma += num
+        cont += 1
+    print('Soma de todos os pares até agora: {}'.format(soma))
+print('Dos {} números pares que você informou, a soma é {}.'.format(cont, soma))
