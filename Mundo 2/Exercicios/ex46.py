c13789df0775ca404e7b2ac0bfbf00c2f0dc3c4e
Gabriel <gabriel@mpsouza.com.br>
2022-03-30 15:27:01 -0300

    2.13.46

--- Mundo 2/Exercicios/ex46.py
+++ Mundo 2/Exercicios/ex46.py
@@ -3,7 +3,8 @@ from time import sleep
 import emoji
 for c in range(10, 0, -1):
     print(c)
-    sleep(1)
-print(emoji.emojize("\033[1;31m:fireworks: \033[m") * 21)
-print('\033[1;31m{}Fogos de artifício {}\033[m'.format(emoji.emojize(":fireworks: ") * 7, emoji.emojize(":fireworks: ") * 7))
-print(emoji.emojize("\033[1;31m:fireworks: \033[m") * 21)
+    sleep(0.45)
+fogos = emoji.emojize("\033[1;31m:fireworks: \033[m") * 11
+print(fogos)
+print('\033[1;31m{}Fogos de artifício {}\033[m'.format(emoji.emojize(":fireworks: ") * 2, emoji.emojize(":fireworks: ") * 2))
+print(fogos)
