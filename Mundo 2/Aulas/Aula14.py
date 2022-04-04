afeb42fa387b945f9fac1e808c99cb59e4d263d5
Gabriel <gabriel@mpsouza.com.br>
2022-04-04 11:42:56 -0300

    2.14.0

---
+++ Mundo 2/Aulas/Aula14.py
@@ -0,0 +1,11 @@
+r = 'S'
+par = impar = 0
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] ')).upper()
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print('Você digitou {} números pares e {} números impares.'.format(par, impar))
