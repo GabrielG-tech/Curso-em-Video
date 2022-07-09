f3f25b7668d8a63b73e9ee029e91b4793960f399
Gabriel <gabriel@mpsouza.com.br>
2022-07-08 21:35:04 -0300

    ex58_feito

--- Mundo 2/Exercicios/ex58.py
+++ Mundo 2/Exercicios/ex58.py
@@ -1,7 +1,9 @@
 print('{:=^40}'.format(' DESAFIO 58 '))
 import random
 numero_aleatorio = random.randint(0, 10)
+num_tentativas = 0
 tentativa = int(input('Tente adivnha o número que estou pensando [0-10]: '))
 while tentativa != numero_aleatorio:
     tentativa = int(input('Ops! Você errou... Tente novamente: '))
-print(f'Parabens você acertou!\nEra {numero_aleatorio} mesmo!')
+    num_tentativas += 1
+print(f'Parabens você acertou!\nCom um total de {num_tentativas} tentativas.\nEra {numero_aleatorio} mesmo!')
