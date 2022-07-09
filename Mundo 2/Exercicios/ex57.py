fd2e29355c4da08e736f2ee3ad05215f3e529e52
Gabriel <gabriel@mpsouza.com.br>
2022-07-08 21:32:30 -0300

    ex57_feito

--- Mundo 2/Exercicios/ex57.py
+++ Mundo 2/Exercicios/ex57.py
@@ -1,1 +1,5 @@
 print('{:=^40}'.format(' DESAFIO 57 '))
+sexo = str(input('Digite seus sexo [M/F]: ')).upper()
+while sexo != 'M' or sexo != 'F':
+    sexo = str(input('Esta resposta é inválida\nPorfavor digite seus sexo [M/F]: ')).upper()
+print(sexo)
--- Mundo 2/Exercicios/ex58.py
+++ Mundo 2/Exercicios/ex58.py
@@ -1,1 +1,7 @@
 print('{:=^40}'.format(' DESAFIO 58 '))
+import random
+numero_aleatorio = random.randint(0, 10)
+tentativa = int(input('Tente adivnha o número que estou pensando [0-10]: '))
+while tentativa != numero_aleatorio:
+    tentativa = int(input('Ops! Você errou... Tente novamente: '))
+print(f'Parabens você acertou!\nEra {numero_aleatorio} mesmo!')
