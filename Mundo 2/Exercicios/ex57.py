4870b993e84bf5f1cc7fd5b96a92eeb08971f5fe
Gabriel <gabriel@mpsouza.com.br>
2022-07-08 23:28:38 -0300

    ex59_fazendo

--- Mundo 2/Exercicios/ex57.py
+++ Mundo 2/Exercicios/ex57.py
@@ -1,4 +1,5 @@
 print('{:=^40}'.format(' DESAFIO 57 '))
+sexo = ''
 sexo = str(input('Digite seus sexo [M/F]: ')).upper()
 while sexo != 'M' or sexo != 'F':
     sexo = str(input('Esta resposta é inválida\nPorfavor digite seus sexo [M/F]: ')).upper()
--- Mundo 2/Exercicios/ex59.py
+++ Mundo 2/Exercicios/ex59.py
@@ -1,2 +1,23 @@
+import os
+import sys
+os.system('cls' if os.name == 'nt' else 'clear')
 print('{:=^40}'.format(' DESAFIO 59 '))
-
+print('Digite dois valores para operação.')
+n1 = float(input('Digite o 1º Número: '))
+n2 = float(input('Digite o 2º Número: '))
+print('''
+[1] somar
+[2] multiplicar
+[3] maior valor
+[4] novos números
+[5] sair do programa''')
+resposta = int(input('Digite sua escolha: '))
+while resposta != 5:
+    if resposta == 1:
+        print(n1, '+', n2, '=', n1 + n2)
+    if resposta == 2:
+        print(n1, 'x', n2, '=', n1 * n2)
+    if resposta == 3:
+        print(max(n1, n2))
+    if resposta == 4:
+        resposta = int(input('Digite sua escolha: '))
